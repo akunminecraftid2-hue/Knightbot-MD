35802e685ab2fc288b3af24edde42607632d7fdb
akunminecraftid2-hue <akunminecraftid2@gmail.com>
2025-11-04 08:07:59 +0800

    settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Zae Bot',
+  author: 'Zae🗿‎',
+  botName: "Zae",
+  botOwner: 'Zae', // Your name
+  ownerNumber: '62882022448085', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  maxStoreMessages: 20, 
+  maxStoreMessages: 50, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.0",
